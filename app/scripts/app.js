80655954edfe055b117e894dcdb80aa04fff2306
Daniel Futerman <daniel.futerman@gmail.com>
2014-05-15 17:21:58 +0200

    Add Donors section page structure and scaffolding

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,12 +6,34 @@ angular.module('bsis', [
 ])
 .config(function($routeProvider) {
 	$routeProvider
+		// HOME PAGE
 		.when('/', {
 			templateUrl : 'views/home.html',
 			controller  : 'HomeCtrl',
 		})
 
+		// DONORS URLs
 		.when('/donors', {
+			templateUrl : 'views/donors.html',
+			controller  : 'DonorsCtrl'
+		})
+		.when('/findDonor', {
+			templateUrl : 'views/donors.html',
+			controller  : 'DonorsCtrl'
+		})
+		.when('/addDonor', {
+			templateUrl : 'views/donors.html',
+			controller  : 'DonorsCtrl'
+		})
+		.when('/linkDonation', {
+			templateUrl : 'views/donors.html',
+			controller  : 'DonorsCtrl'
+		})
+		.when('/manageClinic', {
+			templateUrl : 'views/donors.html',
+			controller  : 'DonorsCtrl'
+		})
+		.when('/exportDonorList', {
 			templateUrl : 'views/donors.html',
 			controller  : 'DonorsCtrl'
 		})
--- app/scripts/controllers/donors.js
+++ app/scripts/controllers/donors.js
@@ -3,12 +3,16 @@
 angular.module('bsis')
 	.controller('DonorsCtrl', function ($scope, $location) {
 
-	$scope.donorSection = 'views/donors/findDonor.html';
+    $scope.selection = '/donors';
 
     $scope.isCurrent = function(path) {
       if (path.length > 1 && $location.path().substr(0, path.length) === path) {
+        $location.path(path)
+        $scope.selection = path;
         return true;
       } else if ($location.path() === path) {
+        return true;
+      } else if ($location.path() === "/donors" && path === "/findDonor") {
         return true;
       } else {
         return false;
