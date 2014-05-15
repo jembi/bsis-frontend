943a971862726310803e3dd7d0525a761e4f914a
Daniel Futerman <daniel.futerman@gmail.com>
2014-05-15 17:42:29 +0200

    Update header menu to match updated URLs

--- app/scripts/controllers/header.js
+++ app/scripts/controllers/header.js
@@ -4,34 +4,15 @@ angular.module('bsis')
   .controller('HeaderCtrl', function ($scope, $location) {
 
 
-			$scope.currentSection = 'HOME';
-			$scope.sectionList = [
-			    {'title': 'DONORS',
-			     'href': '#donors',
-			 	 'glyph': 'glyphicon-tint'},
-			    {'title': 'COMPONENTS',
-			     'href': '#components',
-			     'glyph': 'glyphicon-tasks'},
-			    {'title': 'TESTING',
-			     'href': '#testing',
-			     'glyph': 'glyphicon-inbox'},
-			    {'title': 'INVENTORY',
-			     'href': '#inventory',
-			     'glyph': 'glyphicon-tag'},
-			    {'title': 'REPORTS',
-			     'href': '#reports',
-			     'glyph': 'glyphicon-stats'},
-			    {'title': 'MOBILE CLINIC',
-			     'href': '#mobile',
-			     'glyph': 'glyphicon-road'},
-			    {'title': 'SETTINGS',
-			     'href': '#settings',
-			     'glyph': 'glyphicon-cog'}
-			  ];
-
-	$scope.$on('$locationChangeStart', function(event){
-
-		if(~$location.path().indexOf('donors')){
+  	// set menu on initial load
+	// if on donors page, set menu to DONORS
+		if(~$location.path().indexOf('donors') 
+			|| ~$location.path().indexOf('findDonor') 
+			|| ~$location.path().indexOf('addDonor')
+			|| ~$location.path().indexOf('linkDonation')
+			|| ~$location.path().indexOf('manageClinic')
+			|| ~$location.path().indexOf('exportDonorList')
+		){
 			$scope.currentSection = 'DONORS';
 			$scope.sectionList = [
 			    {'title': 'HOME',
@@ -57,6 +38,71 @@ angular.module('bsis')
 			     'glyph': 'glyphicon-cog'}
 			  ];
 		}
+		// else set menu to HOME
+		else{
+			$scope.currentSection = 'HOME';
+			$scope.sectionList = [
+			    {'title': 'DONORS',
+			     'href': '#donors',
+			 	 'glyph': 'glyphicon-tint'},
+			    {'title': 'COMPONENTS',
+			     'href': '#components',
+			     'glyph': 'glyphicon-tasks'},
+			    {'title': 'TESTING',
+			     'href': '#testing',
+			     'glyph': 'glyphicon-inbox'},
+			    {'title': 'INVENTORY',
+			     'href': '#inventory',
+			     'glyph': 'glyphicon-tag'},
+			    {'title': 'REPORTS',
+			     'href': '#reports',
+			     'glyph': 'glyphicon-stats'},
+			    {'title': 'MOBILE CLINIC',
+			     'href': '#mobile',
+			     'glyph': 'glyphicon-road'},
+			    {'title': 'SETTINGS',
+			     'href': '#settings',
+			     'glyph': 'glyphicon-cog'}
+			  ];
+		}
+
+	//set menu on location change
+	$scope.$on('$locationChangeStart', function(event){
+
+		// if on donors page, set menu to DONORS
+		if(~$location.path().indexOf('donors') 
+			|| ~$location.path().indexOf('findDonor') 
+			|| ~$location.path().indexOf('addDonor')
+			|| ~$location.path().indexOf('linkDonation')
+			|| ~$location.path().indexOf('manageClinic')
+			|| ~$location.path().indexOf('exportDonorList')
+		){
+			$scope.currentSection = 'DONORS';
+			$scope.sectionList = [
+			    {'title': 'HOME',
+			     'href': '#',
+			 	 'glyph': 'glyphicon-home'},
+			    {'title': 'COMPONENTS',
+			     'href': '#components',
+			     'glyph': 'glyphicon-tasks'},
+			    {'title': 'TESTING',
+			     'href': '#testing',
+			     'glyph': 'glyphicon-inbox'},
+			    {'title': 'INVENTORY',
+			     'href': '#inventory',
+			     'glyph': 'glyphicon-tag'},
+			    {'title': 'REPORTS',
+			     'href': '#reports',
+			     'glyph': 'glyphicon-stats'},
+			    {'title': 'MOBILE CLINIC',
+			     'href': '#mobile',
+			     'glyph': 'glyphicon-road'},
+			    {'title': 'SETTINGS',
+			     'href': '#settings',
+			     'glyph': 'glyphicon-cog'}
+			  ];
+		}
+		// else set menu to HOME
 		else{
 			$scope.currentSection = 'HOME';
 			$scope.sectionList = [
