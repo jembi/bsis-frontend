eb097159bc5f127d82a38887cd3a2c6cf10fe65e
Daniel Futerman <daniel.futerman@gmail.com>
2014-05-16 00:31:34 +0200

    Add ngMockE2E based module to mock API calls

---
+++ app/scripts/mockAPI.js
@@ -0,0 +1,19 @@
+var mockAPI = angular.module('mockAPI', [ 'ngMockE2E']);
+  mockAPI.run(function($httpBackend) {
+    
+
+    // login mock
+    $httpBackend.whenPOST('/login', {username: 'admin', password: '123'}).respond(
+      { user:{
+          id: "100",
+          userId: "Super User",
+          role: "admin"
+        }
+      })
+
+    // Don't mock html views
+    $httpBackend.whenGET(/views\/\w+.*/).passThrough();
+    $httpBackend.whenGET(/^\w+.*/).passThrough();
+    $httpBackend.whenPOST(/^\w+.*/).passThrough();
+
+  });
