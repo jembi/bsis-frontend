eea95fa609967fffc4daecae929a14bed4fa45dc
Daniel Futerman <daniel.futerman@gmail.com>
2014-07-04 20:14:54 +0200

    Add Components section to header.js and format spacing

--- app/scripts/controllers/header.js
+++ app/scripts/controllers/header.js
@@ -1,157 +1,212 @@
 'use strict';
 
 angular.module('bsis')
-  .controller('HeaderCtrl', function ($scope, $location, AuthService) {
+.controller('HeaderCtrl', function ($scope, $location, AuthService) {
 
+  // set menu on initial load
+  // if on donors page, set menu to DONORS
+  if(~$location.path().indexOf('donors')        ||
+    ~$location.path().indexOf('findDonor')      ||
+    ~$location.path().indexOf('addDonor')       ||
+    ~$location.path().indexOf('linkDonation')   ||
+    ~$location.path().indexOf('manageClinic')   ||
+    ~$location.path().indexOf('exportDonorList')
+    ){
+      $scope.currentSection = 'DONORS';
+      $scope.sectionList = [
+        {'title': 'HOME',
+         'href': '#home',
+         'glyph': 'glyphicon-home'},
+        {'title': 'COMPONENTS',
+         'href': '#components',
+         'glyph': 'glyphicon-tasks'},
+        {'title': 'TESTING',
+         'href': '#testing',
+         'glyph': 'glyphicon-inbox'},
+        {'title': 'INVENTORY',
+         'href': '#inventory',
+         'glyph': 'glyphicon-tag'},
+        {'title': 'REPORTS',
+         'href': '#reports',
+         'glyph': 'glyphicon-stats'},
+        {'title': 'MOBILE CLINIC',
+         'href': '#mobile',
+         'glyph': 'glyphicon-road'},
+        {'title': 'SETTINGS',
+         'href': '#settings',
+         'glyph': 'glyphicon-cog'}
+      ];
+    }
 
-  	// set menu on initial load
-	// if on donors page, set menu to DONORS
-		if(~$location.path().indexOf('donors') 
-			|| ~$location.path().indexOf('findDonor') 
-			|| ~$location.path().indexOf('addDonor')
-			|| ~$location.path().indexOf('linkDonation')
-			|| ~$location.path().indexOf('manageClinic')
-			|| ~$location.path().indexOf('exportDonorList')
-		){
-			$scope.currentSection = 'DONORS';
-			$scope.sectionList = [
-			    {'title': 'HOME',
-			     'href': '#home',
-			 	 'glyph': 'glyphicon-home'},
-			    {'title': 'COMPONENTS',
-			     'href': '#components',
-			     'glyph': 'glyphicon-tasks'},
-			    {'title': 'TESTING',
-			     'href': '#testing',
-			     'glyph': 'glyphicon-inbox'},
-			    {'title': 'INVENTORY',
-			     'href': '#inventory',
-			     'glyph': 'glyphicon-tag'},
-			    {'title': 'REPORTS',
-			     'href': '#reports',
-			     'glyph': 'glyphicon-stats'},
-			    {'title': 'MOBILE CLINIC',
-			     'href': '#mobile',
-			     'glyph': 'glyphicon-road'},
-			    {'title': 'SETTINGS',
-			     'href': '#settings',
-			     'glyph': 'glyphicon-cog'}
-			  ];
-		}
-		// else set menu to HOME
-		else{
-			$scope.currentSection = 'HOME';
-			$scope.sectionList = [
-			    {'title': 'DONORS',
-			     'href': '#donors',
-			 	 'glyph': 'glyphicon-tint'},
-			    {'title': 'COMPONENTS',
-			     'href': '#components',
-			     'glyph': 'glyphicon-tasks'},
-			    {'title': 'TESTING',
-			     'href': '#testing',
-			     'glyph': 'glyphicon-inbox'},
-			    {'title': 'INVENTORY',
-			     'href': '#inventory',
-			     'glyph': 'glyphicon-tag'},
-			    {'title': 'REPORTS',
-			     'href': '#reports',
-			     'glyph': 'glyphicon-stats'},
-			    {'title': 'MOBILE CLINIC',
-			     'href': '#mobile',
-			     'glyph': 'glyphicon-road'},
-			    {'title': 'SETTINGS',
-			     'href': '#settings',
-			     'glyph': 'glyphicon-cog'}
-			  ];
-		}
+  // else if on components page, set menu to COMPONENTS
+  if(~$location.path().indexOf('components')        ||
+      ~$location.path().indexOf('findComponents')   ||
+      ~$location.path().indexOf('recordComponents')
+    ){
+      $scope.currentSection = 'COMPONENTS';
+      $scope.sectionList = [
+        {'title': 'HOME',
+         'href': '#home',
+         'glyph': 'glyphicon-home'},
+        {'title': 'DONORS',
+         'href': '#donors',
+         'glyph': 'glyphicon-tint'},
+        {'title': 'TESTING',
+         'href': '#testing',
+         'glyph': 'glyphicon-inbox'},
+        {'title': 'INVENTORY',
+         'href': '#inventory',
+         'glyph': 'glyphicon-tag'},
+        {'title': 'REPORTS',
+         'href': '#reports',
+         'glyph': 'glyphicon-stats'},
+        {'title': 'MOBILE CLINIC',
+         'href': '#mobile',
+         'glyph': 'glyphicon-road'},
+        {'title': 'SETTINGS',
+         'href': '#settings',
+         'glyph': 'glyphicon-cog'}
+      ];
+    }
+  // else set menu to HOME
+  else{
+      $scope.currentSection = 'HOME';
+      $scope.sectionList = [
+        {'title': 'DONORS',
+         'href': '#donors',
+         'glyph': 'glyphicon-tint'},
+        {'title': 'COMPONENTS',
+         'href': '#components',
+         'glyph': 'glyphicon-tasks'},
+        {'title': 'TESTING',
+         'href': '#testing',
+         'glyph': 'glyphicon-inbox'},
+        {'title': 'INVENTORY',
+         'href': '#inventory',
+         'glyph': 'glyphicon-tag'},
+        {'title': 'REPORTS',
+         'href': '#reports',
+         'glyph': 'glyphicon-stats'},
+        {'title': 'MOBILE CLINIC',
+         'href': '#mobile',
+         'glyph': 'glyphicon-road'},
+        {'title': 'SETTINGS',
+         'href': '#settings',
+         'glyph': 'glyphicon-cog'}
+      ];
+    }
 
-	//set menu on location change
-	$scope.$on('$locationChangeStart', function(event){
+  //set menu on location change
+  $scope.$on('$locationChangeStart', function(event){
 
-		// if on donors page, set menu to DONORS
-		if(~$location.path().indexOf('donors') 
-			|| ~$location.path().indexOf('findDonor') 
-			|| ~$location.path().indexOf('addDonor')
-			|| ~$location.path().indexOf('linkDonation')
-			|| ~$location.path().indexOf('manageClinic')
-			|| ~$location.path().indexOf('exportDonorList')
-		){
-			$scope.currentSection = 'DONORS';
-			$scope.sectionList = [
-			    {'title': 'HOME',
-			     'href': '#home',
-			 	 'glyph': 'glyphicon-home'},
-			    {'title': 'COMPONENTS',
-			     'href': '#components',
-			     'glyph': 'glyphicon-tasks'},
-			    {'title': 'TESTING',
-			     'href': '#testing',
-			     'glyph': 'glyphicon-inbox'},
-			    {'title': 'INVENTORY',
-			     'href': '#inventory',
-			     'glyph': 'glyphicon-tag'},
-			    {'title': 'REPORTS',
-			     'href': '#reports',
-			     'glyph': 'glyphicon-stats'},
-			    {'title': 'MOBILE CLINIC',
-			     'href': '#mobile',
-			     'glyph': 'glyphicon-road'},
-			    {'title': 'SETTINGS',
-			     'href': '#settings',
-			     'glyph': 'glyphicon-cog'}
-			  ];
-		}
-		// else set menu to HOME
-		else{
-			$scope.currentSection = 'HOME';
-			$scope.sectionList = [
-			    {'title': 'DONORS',
-			     'href': '#donors',
-			 	 'glyph': 'glyphicon-tint'},
-			    {'title': 'COMPONENTS',
-			     'href': '#components',
-			     'glyph': 'glyphicon-tasks'},
-			    {'title': 'TESTING',
-			     'href': '#testing',
-			     'glyph': 'glyphicon-inbox'},
-			    {'title': 'INVENTORY',
-			     'href': '#inventory',
-			     'glyph': 'glyphicon-tag'},
-			    {'title': 'REPORTS',
-			     'href': '#reports',
-			     'glyph': 'glyphicon-stats'},
-			    {'title': 'MOBILE CLINIC',
-			     'href': '#mobile',
-			     'glyph': 'glyphicon-road'},
-			    {'title': 'SETTINGS',
-			     'href': '#settings',
-			     'glyph': 'glyphicon-cog'}
-			  ];
-		}
+    // if on donors page, set menu to DONORS
+    if(~$location.path().indexOf('donors')          ||
+      ~$location.path().indexOf('findDonor')        ||
+      ~$location.path().indexOf('addDonor')         ||
+      ~$location.path().indexOf('linkDonation')     ||
+      ~$location.path().indexOf('manageClinic')     ||
+      ~$location.path().indexOf('exportDonorList')
+    ){
+      $scope.currentSection = 'DONORS';
+      $scope.sectionList = [
+        {'title': 'HOME',
+         'href': '#home',
+         'glyph': 'glyphicon-home'},
+        {'title': 'COMPONENTS',
+         'href': '#components',
+         'glyph': 'glyphicon-tasks'},
+        {'title': 'TESTING',
+         'href': '#testing',
+         'glyph': 'glyphicon-inbox'},
+        {'title': 'INVENTORY',
+         'href': '#inventory',
+         'glyph': 'glyphicon-tag'},
+        {'title': 'REPORTS',
+         'href': '#reports',
+         'glyph': 'glyphicon-stats'},
+        {'title': 'MOBILE CLINIC',
+         'href': '#mobile',
+         'glyph': 'glyphicon-road'},
+        {'title': 'SETTINGS',
+         'href': '#settings',
+         'glyph': 'glyphicon-cog'}
+      ];
+    }
+    
+    // else if on components page, set menu to COMPONENTS
+    if(~$location.path().indexOf('components')        ||
+        ~$location.path().indexOf('findComponents')   ||
+        ~$location.path().indexOf('recordComponents')
+    ){
+        $scope.currentSection = 'COMPONENTS';
+        $scope.sectionList = [
+            {'title': 'HOME',
+             'href': '#home',
+             'glyph': 'glyphicon-home'},
+            {'title': 'DONORS',
+             'href': '#donors',
+             'glyph': 'glyphicon-tint'},
+            {'title': 'TESTING',
+             'href': '#testing',
+             'glyph': 'glyphicon-inbox'},
+            {'title': 'INVENTORY',
+             'href': '#inventory',
+             'glyph': 'glyphicon-tag'},
+            {'title': 'REPORTS',
+             'href': '#reports',
+             'glyph': 'glyphicon-stats'},
+            {'title': 'MOBILE CLINIC',
+             'href': '#mobile',
+             'glyph': 'glyphicon-road'},
+            {'title': 'SETTINGS',
+             'href': '#settings',
+             'glyph': 'glyphicon-cog'}
+          ];
+    }
 
-	});
-
-	$scope.status = {
-		isopen: false
-	};
-
-	$scope.toggled = function(open) {
-		console.log('Dropdown is now: ', open);
-	};
-
-	$scope.toggleDropdown = function($event) {
-		$event.preventDefault();
-		$event.stopPropagation();
-		$scope.status.isopen = !$scope.status.isopen;
-	};
-
-	$scope.logout = function () {
-    	console.log("HEADERJS LOGOOOOOOOOUUUUTTTT");
-    	AuthService.logout();
-    	$location.path( "/" );
-    };
-
-	
+    // else set menu to HOME
+    else{
+      $scope.currentSection = 'HOME';
+      $scope.sectionList = [
+        {'title': 'DONORS',
+         'href': '#donors',
+         'glyph': 'glyphicon-tint'},
+        {'title': 'COMPONENTS',
+         'href': '#components',
+         'glyph': 'glyphicon-tasks'},
+        {'title': 'TESTING',
+         'href': '#testing',
+         'glyph': 'glyphicon-inbox'},
+        {'title': 'INVENTORY',
+         'href': '#inventory',
+         'glyph': 'glyphicon-tag'},
+        {'title': 'REPORTS',
+         'href': '#reports',
+         'glyph': 'glyphicon-stats'},
+        {'title': 'MOBILE CLINIC',
+         'href': '#mobile',
+         'glyph': 'glyphicon-road'},
+        {'title': 'SETTINGS',
+         'href': '#settings',
+         'glyph': 'glyphicon-cog'}
+      ];
+    }
 
   });
+
+  $scope.status = {
+    isopen: false
+  };
+
+  $scope.toggleDropdown = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+    $scope.status.isopen = !$scope.status.isopen;
+  };
+
+  $scope.logout = function () {
+    AuthService.logout();
+    $location.path( "/" );
+  };
+
+});
