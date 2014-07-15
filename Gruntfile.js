6e885a054c080ae5258f8a78962da7a278f3b75a
Daniel Futerman <daniel.futerman@gmail.com>
2014-07-15 21:29:50 +0200

    Configure jshint to exclude 3rd party scripts
    Configure jshint to exclude:
    - bootstrap.min.js
    - jquery.min.js scripts

--- Gruntfile.js
+++ Gruntfile.js
@@ -103,7 +103,9 @@ module.exports = function (grunt) {
       },
       all: [
         'Gruntfile.js',
-        '<%= yeoman.app %>/scripts/{,*/}*.js'
+        '<%= yeoman.app %>/scripts/{,*/}*.js',
+        '!<%= yeoman.app %>/scripts/jquery.min.js',
+        '!<%= yeoman.app %>/scripts/bootstrap.min.js'
       ],
       test: {
         options: {
