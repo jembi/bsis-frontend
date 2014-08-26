6c4ec7f890808ebdbfef673f1b633622550de825
danfuterman <daniel@jembi.org>
2014-08-26 11:23:00 +0200

    Move mockAPI responses out to separate JSON files
    - Use angular-resource $resource to get data from JSON files

--- app/scripts/mockAPI.js
+++ app/scripts/mockAPI.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var mockAPI = angular.module('mockAPI', [ 'ngMockE2E']);
-  mockAPI.run(function($httpBackend) {
+var mockAPI = angular.module('mockAPI', ['ngMockE2E', 'ngResource']);
+  mockAPI.run(function($httpBackend, $resource) {
     
     //console.log("IN MOCKAPI");
 
@@ -15,91 +15,21 @@ var mockAPI = angular.module('mockAPI', [ 'ngMockE2E']);
       });
 
     // findDonor mock (firstName=sample, lastName=donor)
-    $httpBackend.whenGET('/findDonor?firstName=sample&lastName=donor').respond(
-      {
-      "donors": [
-      { 
-        firstName: "Sample",
-        lastName: "Donor",
-        donorNum: "000001",
-        gender: "Male",
-        dob: "10/01/1965"
-      },
-      {
-        firstName: "Sample",
-        lastName: "Donor",
-        donorNum: "000002",
-        gender: "Female",
-        dob: "12/10/1980"
-      },
-      {
-        firstName: "Sample",
-        lastName: "Donor",
-        donorNum: "000003",
-        gender: "Female",
-        dob: "20/01/1975"
-      },
-      {
-        firstName: "Sample",
-        lastName: "Donorific",
-        donorNum: "000004",
-        gender: "Male",
-        dob: "10/01/1975"
-      },
-      {
-        firstName: "Samplee",
-        lastName: "Donor",
-        donorNum: "000005",
-        gender: "Female",
-        dob: "10/05/1975"
-      }
-      ,
-      {
-        firstName: "Sample",
-        lastName: "Donor",
-        donorNum: "000006",
-        gender: "Female",
-        dob: "10/05/1975"
-      },
-      {
-        firstName: "Sample",
-        lastName: "Donor",
-        donorNum: "000007",
-        gender: "Female",
-        dob: "10/05/1980"
-      },
-      {
-        firstName: "Sample",
-        lastName: "Donor",
-        donorNum: "000008",
-        gender: "Female",
-        dob: "10/05/1975"
-      },
-      {
-        firstName: "Sample",
-        lastName: "Donor",
-        donorNum: "000009",
-        gender: "Female",
-        dob: "10/05/1975"
-      },
-      {
-        firstName: "Samplee",
-        lastName: "Donor",
-        donorNum: "000010",
-        gender: "Female",
-        dob: "10/05/1975"
-      },
-      {
-        firstName: "Sample",
-        lastName: "Donor",
-        donorNum: "000011",
-        gender: "Female",
-        dob: "10/05/1990"
-      }
-      ]
-    });
+    $httpBackend.whenGET('/findDonor?firstName=sample&lastName=donor').respond($resource('data/donors.json').get());
     // pass through all other /findDonor requests (will respond with a 404 (Not Found))
     $httpBackend.whenGET(/findDonor?\w+.*/).passThrough();
+
+
+    $httpBackend.whenPOST('/login', {username: 'admin', password: '123'}).respond(
+      { user:{
+          id: "100",
+          userId: "Admin User",
+          role: "admin"
+        }
+      });
+
+    // addDonor mock
+    $httpBackend.whenGET('/addDonor').respond($resource('data/donorformfields.json').get());
 
     // Don't mock html views
     $httpBackend.whenGET(/views\/\w+.*/).passThrough();
