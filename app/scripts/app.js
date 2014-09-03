098fa8f1c81197b3719490788971eb016177accf
Daniel Futerman <daniel.futerman@gmail.com>
2014-09-03 12:51:17 +0200

    Add directive to capitalise the first letter of input fields

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -254,4 +254,23 @@ angular.module('bsis', [
     };
   }])
 
+  /* Custom directive to capitalize the first lettter of input fields */
+  .directive('capitalizeFirstLetter', function($parse) {
+   return {
+     require: 'ngModel',
+     link: function(scope, element, attrs, modelCtrl) {
+          var capitalize = function(inputValue) {
+             var capitalized = inputValue.charAt(0).toUpperCase() + inputValue.substring(1);
+             if(capitalized !== inputValue) {
+                modelCtrl.$setViewValue(capitalized);
+                modelCtrl.$render();
+              }
+              return capitalized;
+          };
+           modelCtrl.$parsers.push(capitalize);
+           capitalize($parse(attrs.ngModel)(scope)); // capitalize first letter
+       }
+   };
+  })
+
 ;
