0db90a86f3e3e10776632f8b84786bb1281e7b1c
Daniel Futerman <daniel.futerman@gmail.com>
2014-09-10 12:07:46 +0200

    Add initial recordComponents controller & view

--- app/scripts/controllers/components.js
+++ app/scripts/controllers/components.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('bsis')
-  .controller('ComponentsCtrl', function ($scope, $location, ComponentsService, ICONS) {
+  .controller('ComponentsCtrl', function ($scope, $location, ComponentService, ICONS, $filter, ngTableParams) {
 
     $scope.icons = ICONS;
 
@@ -18,4 +18,51 @@ angular.module('bsis')
         return false;
       }
     };
-  });
+
+    var data = {};
+    $scope.data = data;
+
+    $scope.componentsSearch = {
+      donationIdentificationNumber: ''
+    };
+    $scope.searchResults = '';
+
+    $scope.clear = function () {
+      $scope.componentsSearch = {};
+      $scope.searchResults = '';
+    };
+
+    $scope.getComponentsByDIN = function () {   
+      ComponentService.getComponentsByDIN($scope.componentsSearch.donationIdentificationNumber).then(function (response) {
+          data = response.data.components;
+          $scope.data = data;
+          console.log("$scope.data: ", $scope.data);
+          $scope.searchResults = true;
+        }, function () {
+          $scope.searchResults = false;
+      });
+
+      $scope.componentsTableParams = new ngTableParams({
+        page: 1,            // show first page
+        count: 6,          // count per page
+        filter: {},
+        sorting: {}
+      }, 
+      {
+        defaultSort: 'asc',
+        counts: [], // hide page counts control
+        total: data.length, // length of data
+        getData: function ($defer, params) {
+          var filteredData = params.filter() ?
+            $filter('filter')(data, params.filter()) : data;
+          var orderedData = params.sorting() ?
+            $filter('orderBy')(filteredData, params.orderBy()) : data;
+          params.total(orderedData.length); // set total for pagination
+          $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+        }
+      });
+
+    };
+
+  })
+;
