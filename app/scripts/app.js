de0572e6bca9f9a032b4ca0a8f5fdba44748e331
Daniel Futerman <daniel.futerman@gmail.com>
2014-09-14 11:29:26 +0200

    Add routing for manageTTITesting view

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -154,6 +154,13 @@ angular.module('bsis', [
           permission: PERMISSIONS.VIEW_TESTING
         }
       })
+      .when('/manageTTITesting', {
+        templateUrl : 'views/testing.html',
+        controller  : 'TTITestBatchCtrl',
+        data: {
+          permission: PERMISSIONS.VIEW_TESTING
+        }
+      })
       .when('/uploadTestResults', {
         templateUrl : 'views/testing.html',
         controller  : 'TestingCtrl',
