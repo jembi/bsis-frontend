51b1528a178b36e2ea919f3c746b0395f09f60a1
Daniel Futerman <daniel.futerman@gmail.com>
2014-09-16 12:12:40 +0200

    Add service methods to get/set donationBatches

--- app/scripts/services/testing.js
+++ app/scripts/services/testing.js
@@ -4,11 +4,13 @@ angular.module('bsis')
 .factory('TestingService', function ($http) {
 
   var testBatchObj = {};
+  var donationBatchesObj = [];
 
   return {
     getTestBatchFormFields: function () {
       return $http.get('/getTestBatchFormFields')
         .success(function(data, status, headers, config){
+          //donationBatchesObj = data.donationBatches;
           return data;
       })
       .error(function(data){
@@ -20,6 +22,12 @@ angular.module('bsis')
     },
     setTestBatch: function(testBatch){
       testBatchObj = testBatch;
+    },
+    getDonationBatches: function(){
+      return donationBatchesObj;
+    },
+    setDonationBatches: function(donationBatch){
+      donationBatchesObj = donationBatch;
     }
   };
 });
