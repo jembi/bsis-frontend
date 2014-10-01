afca1a199621a1934dd6906a864db9f81e4e450d
Daniel Futerman <daniel.futerman@gmail.com>
2014-10-01 22:56:18 +0200

    Add initial api service class

---
+++ app/scripts/services/api.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('bsis')
+
+  .factory('Api', function ($resource, $http, HOST, PORT, APP) {
+
+    var url = 'http://' + APIHOST + ':' + APIPORT + '/' + APIAPP;
+
+    return {
+
+    };
+});
