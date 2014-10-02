f4a7875903ecd8608c0746fa790e491e29dd0d11
Daniel Futerman <daniel.futerman@gmail.com>
2014-10-02 08:55:22 +0200

    Add GET /user API method

--- app/scripts/services/api.js
+++ app/scripts/services/api.js
@@ -2,11 +2,19 @@
 
 angular.module('bsis')
 
-  .factory('Api', function ($resource, $http, HOST, PORT, APP) {
+  .factory('Api', function ($resource, $http, APIHOST, APIPORT, APIAPP) {
 
     var url = 'http://' + APIHOST + ':' + APIPORT + '/' + APIAPP;
 
     return {
+      User: $resource(url + '/user' , {}, {
+        get: {
+          method: 'GET'
+         // headers: {'Authorization': 'Basic c3VwZXJ1c2VyOnN1cGVydXNlcg=='}
+        }
+      })
+
+
 
     };
 });
