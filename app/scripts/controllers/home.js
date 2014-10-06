92f765b16681e5b7e8ccb2d4fe90e6c0a6c6280d
Daniel Futerman <daniel.futerman@gmail.com>
2014-10-06 12:05:31 +0200

    Add UI permissions to home page

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -1,6 +1,7 @@
 'use strict';
 
 angular.module('bsis')
-  .controller('HomeCtrl', function ($scope, ICONS) {
+  .controller('HomeCtrl', function ($scope, ICONS, PERMISSIONS) {
     $scope.icons = ICONS;
+    $scope.permissions = PERMISSIONS;
   });
