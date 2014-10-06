9cb03973a86ae87b032001cd394e83f46c222f72
Daniel Futerman <daniel.futerman@gmail.com>
2014-10-06 14:17:31 +0200

    Set standard header drop-down sectionList

--- app/scripts/controllers/header.js
+++ app/scripts/controllers/header.js
@@ -5,6 +5,36 @@ angular.module('bsis')
 
   $scope.icons = ICONS;
 
+  $scope.sectionList = [
+    {'title': 'HOME',
+     'href': '#home',
+     'icon': ICONS.HOME},
+    {'title': 'DONORS',
+     'href': '#donors',
+     'icon': ICONS.DONORS},
+    {'title': 'COMPONENTS',
+     'href': '#components',
+     'icon': ICONS.COMPONENTS},
+    {'title': 'TESTING',
+     'href': '#testing',
+     'icon': ICONS.TESTING},
+    {'title': 'INVENTORY',
+     'href': '#inventory',
+     'icon': ICONS.INVENTORY},
+    {'title': 'LABELLING',
+     'href': '#labelling',
+     'icon': ICONS.LABELLING},
+    {'title': 'REPORTS',
+     'href': '#reports',
+     'icon': ICONS.REPORTS},
+    {'title': 'MOBILE CLINIC',
+     'href': '#mobile',
+     'icon': ICONS.MOBILE},
+    {'title': 'SETTINGS',
+     'href': '#settings',
+     'icon': ICONS.SETTINGS}
+  ];
+
   // set menu on initial load
   // if on donors page, set menu to DONORS
   if(~$location.path().indexOf('donors')        ||
@@ -16,32 +46,7 @@ angular.module('bsis')
     ~$location.path().indexOf('exportDonorList')
     ){
       $scope.currentSection = 'DONORS';
-      $scope.sectionList = [
-        {'title': 'HOME',
-         'href': '#home',
-         'icon': ICONS.HOME},
-        {'title': 'COMPONENTS',
-         'href': '#components',
-         'icon': ICONS.COMPONENTS},
-        {'title': 'TESTING',
-         'href': '#testing',
-         'icon': ICONS.TESTING},
-        {'title': 'INVENTORY',
-         'href': '#inventory',
-         'icon': ICONS.INVENTORY},
-        {'title': 'LABELLING',
-         'href': '#labelling',
-         'icon': ICONS.LABELLING},
-        {'title': 'REPORTS',
-         'href': '#reports',
-         'icon': ICONS.REPORTS},
-        {'title': 'MOBILE CLINIC',
-         'href': '#mobile',
-         'icon': ICONS.MOBILE},
-        {'title': 'SETTINGS',
-         'href': '#settings',
-         'icon': ICONS.SETTINGS}
-      ];
+      
     }
 
   // else if on components page, set menu to COMPONENTS
@@ -52,32 +57,6 @@ angular.module('bsis')
       ~$location.path().indexOf('discardComponents')
     ){
       $scope.currentSection = 'COMPONENTS';
-      $scope.sectionList = [
-        {'title': 'HOME',
-         'href': '#home',
-         'icon': ICONS.HOME},
-        {'title': 'DONORS',
-         'href': '#donors',
-         'icon': ICONS.DONORS},
-        {'title': 'TESTING',
-         'href': '#testing',
-         'icon': ICONS.TESTING},
-        {'title': 'INVENTORY',
-         'href': '#inventory',
-         'icon': ICONS.INVENTORY},
-        {'title': 'LABELLING',
-         'href': '#labelling',
-         'icon': ICONS.LABELLING},
-        {'title': 'REPORTS',
-         'href': '#reports',
-         'icon': ICONS.REPORTS},
-        {'title': 'MOBILE CLINIC',
-         'href': '#mobile',
-         'icon': ICONS.MOBILE},
-        {'title': 'SETTINGS',
-         'href': '#settings',
-         'icon': ICONS.SETTINGS}
-      ];
     }
 
   // else if on testing page, set menu to TESTING
@@ -91,32 +70,6 @@ angular.module('bsis')
       ~$location.path().indexOf('uploadTestResults')
     ){
       $scope.currentSection = 'TESTING';
-      $scope.sectionList = [
-        {'title': 'HOME',
-         'href': '#home',
-         'icon': ICONS.HOME},
-        {'title': 'DONORS',
-         'href': '#donors',
-         'icon': ICONS.DONORS},
-        {'title': 'COMPONENTS',
-         'href': '#components',
-         'icon': ICONS.COMPONENTS},
-        {'title': 'INVENTORY',
-         'href': '#inventory',
-         'icon': ICONS.INVENTORY},
-        {'title': 'LABELLING',
-         'href': '#labelling',
-         'icon': ICONS.LABELLING},
-        {'title': 'REPORTS',
-         'href': '#reports',
-         'icon': ICONS.REPORTS},
-        {'title': 'MOBILE CLINIC',
-         'href': '#mobile',
-         'icon': ICONS.MOBILE},
-        {'title': 'SETTINGS',
-         'href': '#settings',
-         'icon': ICONS.SETTINGS}
-      ];
     }
 
   // else if on inventory page, set menu to INVENTORY
@@ -127,32 +80,6 @@ angular.module('bsis')
       ~$location.path().indexOf('componentUsage')
     ){
       $scope.currentSection = 'INVENTORY';
-      $scope.sectionList = [
-        {'title': 'HOME',
-         'href': '#home',
-         'icon': ICONS.HOME},
-        {'title': 'DONORS',
-         'href': '#donors',
-         'icon': ICONS.DONORS},
-        {'title': 'COMPONENTS',
-         'href': '#components',
-         'icon': ICONS.COMPONENTS},
-        {'title': 'TESTING',
-         'href': '#testing',
-         'icon': ICONS.TESTING},
-        {'title': 'LABELLING',
-         'href': '#labelling',
-         'icon': ICONS.LABELLING},
-        {'title': 'REPORTS',
-         'href': '#reports',
-         'icon': ICONS.REPORTS},
-        {'title': 'MOBILE CLINIC',
-         'href': '#mobile',
-         'icon': ICONS.MOBILE},
-        {'title': 'SETTINGS',
-         'href': '#settings',
-         'icon': ICONS.SETTINGS}
-      ];
     }
 
   // else if on labelling page, set menu to LABELLING
@@ -160,159 +87,29 @@ angular.module('bsis')
       ~$location.path().indexOf('labelComponents')
     ){
       $scope.currentSection = 'LABELLING';
-      $scope.sectionList = [
-        {'title': 'HOME',
-         'href': '#home',
-         'icon': ICONS.HOME},
-        {'title': 'DONORS',
-         'href': '#donors',
-         'icon': ICONS.DONORS},
-        {'title': 'COMPONENTS',
-         'href': '#components',
-         'icon': ICONS.COMPONENTS},
-        {'title': 'TESTING',
-         'href': '#testing',
-         'icon': ICONS.TESTING},
-        {'title': 'INVENTORY',
-         'href': '#inventory',
-         'icon': ICONS.INVENTORY},
-        {'title': 'REPORTS',
-         'href': '#reports',
-         'icon': ICONS.REPORTS},
-        {'title': 'MOBILE CLINIC',
-         'href': '#mobile',
-         'icon': ICONS.MOBILE},
-        {'title': 'SETTINGS',
-         'href': '#settings',
-         'icon': ICONS.SETTINGS}
-      ];
     }
 
   // else if on reports page, set menu to REPORTS
   else if(~$location.path().indexOf('reports')
     ){
       $scope.currentSection = 'REPORTS';
-      $scope.sectionList = [
-        {'title': 'HOME',
-         'href': '#home',
-         'icon': ICONS.HOME},
-        {'title': 'DONORS',
-         'href': '#donors',
-         'icon': ICONS.DONORS},
-        {'title': 'COMPONENTS',
-         'href': '#components',
-         'icon': ICONS.COMPONENTS},
-        {'title': 'TESTING',
-         'href': '#testing',
-         'icon': ICONS.TESTING},
-        {'title': 'INVENTORY',
-         'href': '#inventory',
-         'icon': ICONS.INVENTORY},
-        {'title': 'LABELLING',
-         'href': '#labelling',
-         'icon': ICONS.LABELLING},
-        {'title': 'MOBILE CLINIC',
-         'href': '#mobile',
-         'icon': ICONS.MOBILE},
-        {'title': 'SETTINGS',
-         'href': '#settings',
-         'icon': ICONS.SETTINGS}
-      ];
     }
 
   // else if on mobile clinic page, set menu to MOBILE
   else if(~$location.path().indexOf('mobile')
     ){
       $scope.currentSection = 'MOBILE';
-      $scope.sectionList = [
-        {'title': 'HOME',
-         'href': '#home',
-         'icon': ICONS.HOME},
-        {'title': 'DONORS',
-         'href': '#donors',
-         'icon': ICONS.DONORS},
-        {'title': 'COMPONENTS',
-         'href': '#components',
-         'icon': ICONS.COMPONENTS},
-        {'title': 'TESTING',
-         'href': '#testing',
-         'icon': ICONS.TESTING},
-        {'title': 'INVENTORY',
-         'href': '#inventory',
-         'icon': ICONS.INVENTORY},
-        {'title': 'LABELLING',
-         'href': '#labelling',
-         'icon': ICONS.LABELLING},
-        {'title': 'REPORTS',
-         'href': '#reports',
-         'icon': ICONS.REPORTS},
-        {'title': 'SETTINGS',
-         'href': '#settings',
-         'icon': ICONS.SETTINGS}
-      ];
     }
 
   // else if on settings page, set menu to SETTINGS
   else if(~$location.path().indexOf('settings')
     ){
       $scope.currentSection = 'SETTINGS';
-      $scope.sectionList = [
-        {'title': 'HOME',
-         'href': '#home',
-         'icon': ICONS.HOME},
-        {'title': 'DONORS',
-         'href': '#donors',
-         'icon': ICONS.DONORS},
-        {'title': 'COMPONENTS',
-         'href': '#components',
-         'icon': ICONS.COMPONENTS},
-        {'title': 'TESTING',
-         'href': '#testing',
-         'icon': ICONS.TESTING},
-        {'title': 'INVENTORY',
-         'href': '#inventory',
-         'icon': ICONS.INVENTORY},
-        {'title': 'LABELLING',
-         'href': '#labelling',
-         'icon': ICONS.LABELLING},
-        {'title': 'REPORTS',
-         'href': '#reports',
-         'icon': ICONS.REPORTS},
-        {'title': 'MOBILE CLINIC',
-         'href': '#mobile',
-         'icon': ICONS.MOBILE}
-      ];
     }
 
   // else set menu to HOME
   else{
       $scope.currentSection = 'HOME';
-      $scope.sectionList = [
-        {'title': 'DONORS',
-         'href': '#donors',
-         'icon': ICONS.DONORS},
-        {'title': 'COMPONENTS',
-         'href': '#components',
-         'icon': ICONS.COMPONENTS},
-        {'title': 'TESTING',
-         'href': '#testing',
-         'icon': ICONS.TESTING},
-        {'title': 'INVENTORY',
-         'href': '#inventory',
-         'icon': ICONS.INVENTORY},
-        {'title': 'LABELLING',
-         'href': '#labelling',
-         'icon': ICONS.LABELLING},
-        {'title': 'REPORTS',
-         'href': '#reports',
-         'icon': ICONS.REPORTS},
-        {'title': 'MOBILE CLINIC',
-         'href': '#mobile',
-         'icon': ICONS.MOBILE},
-        {'title': 'SETTINGS',
-         'href': '#settings',
-         'icon': ICONS.SETTINGS}
-      ];
     }
 
   //set menu on location change
@@ -328,32 +125,6 @@ angular.module('bsis')
       ~$location.path().indexOf('exportDonorList')
     ){
       $scope.currentSection = 'DONORS';
-      $scope.sectionList = [
-        {'title': 'HOME',
-         'href': '#home',
-         'icon': ICONS.HOME},
-        {'title': 'COMPONENTS',
-         'href': '#components',
-         'icon': ICONS.COMPONENTS},
-        {'title': 'TESTING',
-         'href': '#testing',
-         'icon': ICONS.TESTING},
-        {'title': 'INVENTORY',
-         'href': '#inventory',
-         'icon': ICONS.INVENTORY},
-        {'title': 'LABELLING',
-         'href': '#labelling',
-         'icon': ICONS.LABELLING},
-        {'title': 'REPORTS',
-         'href': '#reports',
-         'icon': ICONS.REPORTS},
-        {'title': 'MOBILE CLINIC',
-         'href': '#mobile',
-         'icon': ICONS.MOBILE},
-        {'title': 'SETTINGS',
-         'href': '#settings',
-         'icon': ICONS.SETTINGS}
-      ];
     }
     
     // else if on components page, set menu to COMPONENTS
@@ -364,32 +135,6 @@ angular.module('bsis')
         ~$location.path().indexOf('discardComponents')
     ){
       $scope.currentSection = 'COMPONENTS';
-      $scope.sectionList = [
-        {'title': 'HOME',
-         'href': '#home',
-         'icon': ICONS.HOME},
-        {'title': 'DONORS',
-         'href': '#donors',
-         'icon': ICONS.DONORS},
-        {'title': 'TESTING',
-         'href': '#testing',
-         'icon': ICONS.TESTING},
-        {'title': 'INVENTORY',
-         'href': '#inventory',
-         'icon': ICONS.INVENTORY},
-        {'title': 'LABELLING',
-         'href': '#labelling',
-         'icon': ICONS.LABELLING},
-        {'title': 'REPORTS',
-         'href': '#reports',
-         'icon': ICONS.REPORTS},
-        {'title': 'MOBILE CLINIC',
-         'href': '#mobile',
-         'icon': ICONS.MOBILE},
-        {'title': 'SETTINGS',
-         'href': '#settings',
-         'icon': ICONS.SETTINGS}
-      ];
     }
 
     // else if on testing page, set menu to TESTING
@@ -403,32 +148,6 @@ angular.module('bsis')
       ~$location.path().indexOf('uploadTestResults')
     ){
       $scope.currentSection = 'TESTING';
-      $scope.sectionList = [
-        {'title': 'HOME',
-         'href': '#home',
-         'icon': ICONS.HOME},
-        {'title': 'DONORS',
-         'href': '#donors',
-         'icon': ICONS.DONORS},
-        {'title': 'COMPONENTS',
-         'href': '#components',
-         'icon': ICONS.COMPONENTS},
-        {'title': 'INVENTORY',
-         'href': '#inventory',
-         'icon': ICONS.INVENTORY},
-        {'title': 'LABELLING',
-         'href': '#labelling',
-         'icon': ICONS.LABELLING},
-        {'title': 'REPORTS',
-         'href': '#reports',
-         'icon': ICONS.REPORTS},
-        {'title': 'MOBILE CLINIC',
-         'href': '#mobile',
-         'icon': ICONS.MOBILE},
-        {'title': 'SETTINGS',
-         'href': '#settings',
-         'icon': ICONS.SETTINGS}
-      ];
     }
 
       // else if on inventory page, set menu to INVENTORY
@@ -439,32 +158,6 @@ angular.module('bsis')
         ~$location.path().indexOf('componentUsage')
       ){
         $scope.currentSection = 'INVENTORY';
-        $scope.sectionList = [
-          {'title': 'HOME',
-           'href': '#home',
-           'icon': ICONS.HOME},
-          {'title': 'DONORS',
-           'href': '#donors',
-           'icon': ICONS.DONORS},
-          {'title': 'COMPONENTS',
-           'href': '#components',
-           'icon': ICONS.COMPONENTS},
-          {'title': 'TESTING',
-           'href': '#testing',
-           'icon': ICONS.TESTING},
-          {'title': 'LABELLING',
-           'href': '#labelling',
-           'icon': ICONS.LABELLING},
-          {'title': 'REPORTS',
-           'href': '#reports',
-           'icon': ICONS.REPORTS},
-          {'title': 'MOBILE CLINIC',
-           'href': '#mobile',
-           'icon': ICONS.MOBILE},
-          {'title': 'SETTINGS',
-           'href': '#settings',
-           'icon': ICONS.SETTINGS}
-        ];
       }
 
       // else if on labelling page, set menu to LABELLING
@@ -472,159 +165,29 @@ angular.module('bsis')
         ~$location.path().indexOf('labelComponents')
       ){
         $scope.currentSection = 'LABELLING';
-        $scope.sectionList = [
-          {'title': 'HOME',
-           'href': '#home',
-           'icon': ICONS.HOME},
-          {'title': 'DONORS',
-           'href': '#donors',
-           'icon': ICONS.DONORS},
-          {'title': 'COMPONENTS',
-           'href': '#components',
-           'icon': ICONS.COMPONENTS},
-          {'title': 'TESTING',
-           'href': '#testing',
-           'icon': ICONS.TESTING},
-          {'title': 'INVENTORY',
-           'href': '#inventory',
-           'icon': ICONS.INVENTORY},
-          {'title': 'REPORTS',
-           'href': '#reports',
-           'icon': ICONS.REPORTS},
-          {'title': 'MOBILE CLINIC',
-           'href': '#mobile',
-           'icon': ICONS.MOBILE},
-          {'title': 'SETTINGS',
-           'href': '#settings',
-           'icon': ICONS.SETTINGS}
-        ];
       }
 
     // else if on reports page, set menu to REPORTS
     else if(~$location.path().indexOf('reports')
       ){
         $scope.currentSection = 'REPORTS';
-        $scope.sectionList = [
-          {'title': 'HOME',
-           'href': '#home',
-           'icon': ICONS.HOME},
-          {'title': 'DONORS',
-           'href': '#donors',
-           'icon': ICONS.DONORS},
-          {'title': 'COMPONENTS',
-           'href': '#components',
-           'icon': ICONS.COMPONENTS},
-          {'title': 'TESTING',
-           'href': '#testing',
-           'icon': ICONS.TESTING},
-          {'title': 'INVENTORY',
-           'href': '#inventory',
-           'icon': ICONS.INVENTORY},
-          {'title': 'LABELLING',
-           'href': '#labelling',
-           'icon': ICONS.LABELLING},
-          {'title': 'MOBILE CLINIC',
-           'href': '#mobile',
-           'icon': ICONS.MOBILE},
-          {'title': 'SETTINGS',
-           'href': '#settings',
-           'icon': ICONS.SETTINGS}
-        ];
       }
 
     // else if on mobile clinic page, set menu to MOBILE
     else if(~$location.path().indexOf('mobile')
       ){
         $scope.currentSection = 'MOBILE';
-        $scope.sectionList = [
-          {'title': 'HOME',
-           'href': '#home',
-           'icon': ICONS.HOME},
-          {'title': 'DONORS',
-           'href': '#donors',
-           'icon': ICONS.DONORS},
-          {'title': 'COMPONENTS',
-           'href': '#components',
-           'icon': ICONS.COMPONENTS},
-          {'title': 'TESTING',
-           'href': '#testing',
-           'icon': ICONS.TESTING},
-          {'title': 'INVENTORY',
-           'href': '#inventory',
-           'icon': ICONS.INVENTORY},
-          {'title': 'LABELLING',
-           'href': '#labelling',
-           'icon': ICONS.LABELLING},
-          {'title': 'REPORTS',
-           'href': '#reports',
-           'icon': ICONS.REPORTS},
-          {'title': 'SETTINGS',
-           'href': '#settings',
-           'icon': ICONS.SETTINGS}
-        ];
       }
 
     // else if on settings page, set menu to SETTINGS
     else if(~$location.path().indexOf('settings')
       ){
         $scope.currentSection = 'SETTINGS';
-        $scope.sectionList = [
-          {'title': 'HOME',
-           'href': '#home',
-           'icon': ICONS.HOME},
-          {'title': 'DONORS',
-           'href': '#donors',
-           'icon': ICONS.DONORS},
-          {'title': 'COMPONENTS',
-           'href': '#components',
-           'icon': ICONS.COMPONENTS},
-          {'title': 'TESTING',
-           'href': '#testing',
-           'icon': ICONS.TESTING},
-          {'title': 'INVENTORY',
-           'href': '#inventory',
-           'icon': ICONS.INVENTORY},
-          {'title': 'LABELLING',
-           'href': '#labelling',
-           'icon': ICONS.LABELLING},
-          {'title': 'REPORTS',
-           'href': '#reports',
-           'icon': ICONS.REPORTS},
-          {'title': 'MOBILE CLINIC',
-           'href': '#mobile',
-           'icon': ICONS.MOBILE}
-        ];
       }
 
     // else set menu to HOME
     else{
       $scope.currentSection = 'HOME';
-      $scope.sectionList = [
-        {'title': 'DONORS',
-         'href': '#donors',
-         'icon': ICONS.DONORS},
-        {'title': 'COMPONENTS',
-         'href': '#components',
-         'icon': ICONS.COMPONENTS},
-        {'title': 'TESTING',
-         'href': '#testing',
-         'icon': ICONS.TESTING},
-        {'title': 'INVENTORY',
-         'href': '#inventory',
-         'icon': ICONS.INVENTORY},
-        {'title': 'LABELLING',
-         'href': '#labelling',
-         'icon': ICONS.LABELLING},
-        {'title': 'REPORTS',
-         'href': '#reports',
-         'icon': ICONS.REPORTS},
-        {'title': 'MOBILE CLINIC',
-         'href': '#mobile',
-         'icon': ICONS.MOBILE},
-        {'title': 'SETTINGS',
-         'href': '#settings',
-         'icon': ICONS.SETTINGS}
-      ];
     }
 
   });
