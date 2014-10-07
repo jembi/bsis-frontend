2c320a857bf2b9e6194c1abf607f9eec14f620ed
Daniel Futerman <daniel.futerman@gmail.com>
2014-10-07 11:12:01 +0200

    Update MONTH constant to use string & num values

--- app/scripts/util/constants.js
+++ app/scripts/util/constants.js
@@ -49,7 +49,56 @@ angular.module('bsis')
     options: ["O+", "O-", "A+", "A-", "B+", "B-", "AB+", "AB-"]
   })
   .constant('MONTH', {
-    options: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    "options": [
+    {
+      "id": 1,
+      "name": "January"
+    },
+    {
+      "id": 2,
+      "name": "February"
+    },
+    {
+      "id": 3,
+      "name": "March"
+    },
+    {
+      "id": 4,
+      "name": "April"
+    },
+    {
+      "id": 5,
+      "name": "May"
+    },
+    {
+      "id": 6,
+      "name": "June"
+    },
+    {
+      "id": 7,
+      "name": "July"
+    },
+    {
+      "id": 8,
+      "name": "August"
+    },
+    {
+      "id": 9,
+      "name": "September"
+    },
+    {
+      "id": 10,
+      "name": "October"
+    },
+    {
+      "id": 11,
+      "name": "November"
+    },
+    {
+      "id": 12,
+      "name": "December"
+    }
+  ]
   })
   .constant('TITLE', {
     options: ["Mr","Mrs","Ms","Dr","Prof"]
