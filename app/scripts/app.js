0f1f13a31cdde82639de2cf86d1ebee48b7c9bf0
Daniel Futerman <daniel.futerman@gmail.com>
2014-11-23 14:42:16 +0200

    Add selectOnFocus directive

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -408,4 +408,15 @@ angular.module('bsis', [
     };
   })
 
+  .directive('selectOnFocus', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            element.on('click', function () {
+                this.select();
+            });
+        }
+    };
+  })
+
 ;
