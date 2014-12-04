f66fc35e85d4b5703bd8c92e317115bd7a5c3875
Daniel Futerman <daniel.futerman@gmail.com>
2014-12-04 12:03:14 +0200

    Add initial time-related fields to donation controller

--- app/scripts/controllers/donors.js
+++ app/scripts/controllers/donors.js
@@ -662,6 +662,15 @@ angular.module('bsis')
     var data = {};
     $scope.data = data;
 
+    $scope.startTime = new Date();
+    $scope.endTime = new Date();
+    $scope.hstep = 1;
+    $scope.mstep = 5;
+    $scope.options = {
+      hstep: [1, 2, 3],
+      mstep: [1, 5, 10, 15, 25, 30]
+    };
+
     $scope.init = function () {
       $scope.donationBatch = DonorService.getDonationBatch();
       data = $scope.donationBatch.collectionsInBatch;
