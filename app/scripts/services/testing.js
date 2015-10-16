20aaf0fa555d767517b5e7ca8cda436df333d159
Brett Ausmeier <brett.ausmeier@jembi.org>
2015-10-16 10:07:41 +0200

    Clean up test batch search api method

--- app/scripts/services/testing.js
+++ app/scripts/services/testing.js
@@ -90,7 +90,7 @@ angular.module('bsis')
       });
     },
     getOpenTestBatches: function (response) {
-      Api.FindTestBatches.query({status:'OPEN'}, function (testBatches) {
+      Api.FindTestBatches.get({status:'OPEN'}, function (testBatches) {
         response(testBatches);
         console.log("testBatches: ", testBatches);
       }, function (){
