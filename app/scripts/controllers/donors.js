6ba8b90158102f716d5b8623651ec55f37df37b7
Duma Mtungwa <dumakude@gmail.com>
2015-12-18 10:08:03 +0200

    closes #319

--- app/scripts/controllers/donors.js
+++ app/scripts/controllers/donors.js
@@ -398,7 +398,15 @@ angular.module('bsis')
     $scope.pulseMin = DONATION.DONOR.PULSE_MIN;
     $scope.pulseMax = DONATION.DONOR.PULSE_MAX;
 
-
+    $scope.compareDates = function (deferredUntil) {
+      var perm = new Date(2100, 0, 1);
+      var deferred = moment(deferredUntil).toDate();
+      if (deferred >= perm){
+        return true;
+      } else {
+        return false;
+      }
+    };
 
     DonorService.getDonorFormFields(function(response){
       if (response !== false){
