dc0efb39d593b9a0ab5e5a0ecd5695294fc655a2
Brett Ausmeier <(none)>
2016-01-21 14:44:40 +0200

    Remove quiet option now that all errors are fixed
    The build will still run if there are warnings.

--- Gruntfile.js
+++ Gruntfile.js
@@ -98,9 +98,6 @@ module.exports = function (grunt) {
 
     // Make sure code styles are up to par and there are no obvious mistakes
     eslint: {
-      options: {
-        quiet: true
-      },
       src: [
         'Gruntfile.js',
         '<%= yeoman.app %>/scripts/{,*/}*.js',
