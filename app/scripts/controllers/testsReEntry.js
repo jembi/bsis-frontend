6497e10290bf98c8e9e503cb9a098098640f21e3
lauravignoli <vignoli.laura@gmail.com>
2016-02-22 12:01:24 +0200

    BSIS-447 Show the confirmation popup when the save button is clicked

--- app/scripts/controllers/testsReEntry.js
+++ app/scripts/controllers/testsReEntry.js
@@ -20,7 +20,7 @@ angular.module('bsis')
       });
     };
 
-    $scope.getTestNames = function() {
+    var getTestNames = function() {
       var bloodTestType = $routeParams.bloodTestType;
       if (bloodTestType === 'BASIC_TTI') {
         TestingService.getTTITestingFormFields(function(response) {
@@ -31,20 +31,22 @@ angular.module('bsis')
       }
     };
 
-    var getAllTestOutcomes = function() {
+    // The array reEnteredTestOutcomes is originally populated with all test outcomes where reEntryRequired is false.
+    // As the reEntry values are selected from the dropdowns, they are added to this array.
+    var getReEnteredTestOutcomes = function() {
       $scope.searching = true;
       TestingService.getTestOutcomesByBatchIdAndBloodTestType($routeParams.id, $routeParams.bloodTestType, function(response) {
         if (response !== false) {
           data = response.testResults;
           $scope.data = data;
-          $scope.allTestOutcomes = {};
+          $scope.reEnteredTestOutcomes = {};
 
           angular.forEach($scope.data, function(donationResults) {
             var din = donationResults.donation.donationIdentificationNumber;
-            $scope.allTestOutcomes[din] = {'donationIdentificationNumber': din, 'testResults': {}};
+            $scope.reEnteredTestOutcomes[din] = {'donationIdentificationNumber': din, 'testResults': {}};
             angular.forEach(donationResults.recentTestResults, function(test) {
               if (test.reEntryRequired === false) {
-                $scope.allTestOutcomes[din].testResults[test.bloodTest.id] = test.result;
+                $scope.reEnteredTestOutcomes[din].testResults[test.bloodTest.id] = test.result;
               }
             });
           });
@@ -54,15 +56,50 @@ angular.module('bsis')
     };
 
     getCurrentTestBatch();
-    $scope.getTestNames();
-    getAllTestOutcomes();
+    getTestNames();
+    getReEnteredTestOutcomes();
 
-    var saveTestResults = function(testResults, reEntry) {
+    var reEntriesConfirmed = 0;
+    var reEntriesWithDiscrepancies = 0;
+    var totalReEntries = 0;
+    $scope.reEntryConfirmations = {};
 
-      $scope.savingTestResults = true;
+    var calculateConfirmationCounters = function() {
+      reEntriesConfirmed = 0;
+      reEntriesWithDiscrepancies = 0;
+      totalReEntries = 0;
+      angular.forEach($scope.data, function(donationResults) {
+        var din = donationResults.donation.donationIdentificationNumber;
+        angular.forEach(donationResults.recentTestResults, function(testOutcome) {
+
+          // only check outcomes where reEntryRequired = true
+          if (testOutcome.reEntryRequired === true) {
+            totalReEntries++;
+
+            var confirmed = false;
+            if ($scope.reEntryConfirmations[din] && $scope.reEntryConfirmations[din][testOutcome.bloodTest.id]) {
+              confirmed = $scope.reEntryConfirmations[din][testOutcome.bloodTest.id];
+            }
+            if (testOutcome.result === $scope.reEnteredTestOutcomes[din].testResults[testOutcome.bloodTest.id]) {
+              reEntriesConfirmed++;
+            } else {
+              if (confirmed) {
+                reEntriesConfirmed++;
+              // if it hasn't been confirmed and the result has been selected (is not undefined) it's a discrepancy
+              } else if ($scope.reEnteredTestOutcomes[din].testResults[testOutcome.bloodTest.id]) {
+                reEntriesWithDiscrepancies++;
+              }
+            }
+          }
+        });
+      });
+    };
+
+    var saveTestOutcomes = function() {
+      $scope.savingTestOutcomes = true;
       var requests = [];
-      angular.forEach(testResults, function(value) {
-        var request = TestingService.saveTestResults(value, reEntry, angular.noop);
+      angular.forEach($scope.reEnteredTestOutcomes, function(value) {
+        var request = TestingService.saveTestResults(value, true, angular.noop);
         requests.push(request);
       });
 
@@ -70,16 +107,49 @@ angular.module('bsis')
       $q.all(requests).then(function() {
         $location.path('/viewTestBatch/' + $routeParams.id);
       }).finally(function() {
-        $scope.savingTestResults = false;
+        $scope.savingTestOutcomes = false;
       });
 
     };
 
-    $scope.validateForm = function(testResults, reEntry) {
-      saveTestResults(testResults, reEntry);
+    var confirmSaveTestOutcomes = function() {
+      var messageText = '';
+      messageText += 'Re-entry completed for ' + reEntriesConfirmed  + ' of ' + totalReEntries + ' outcomes.<br /> ';
+      messageText += 'Re-entry required for ' + (totalReEntries - reEntriesConfirmed) + ' of ' + totalReEntries + ' outcomes. <br /> ';
+      var saveObject = {
+        title: 'Save test outcomes',
+        button: 'Save',
+        message: messageText
+      };
+      var modalInstance = $modal.open({
+        animation: false,
+        templateUrl: 'views/confirmModal.html',
+        controller: 'ConfirmModalCtrl',
+        resolve: {
+          confirmObject: function() {
+            return saveObject;
+          }
+        }
+      });
+      modalInstance.result.then(function() {
+        // Then save the test outcomes
+        saveTestOutcomes($scope.reEnteredTestOutcomes, true);
+      }, function() {
+        // save cancelled - do nothing
+      });
     };
 
-    $scope.confirmedCheckbox = false;
+    $scope.validateSaveTestOutcomesForm = function() {
+      calculateConfirmationCounters();
+      
+      // Only save once all the discrepancies have been confirmed
+      if (reEntriesWithDiscrepancies > 0) {
+        $scope.showAlert = true;
+      } else {
+        $scope.showAlert = false;
+        confirmSaveTestOutcomes();
+      }
+    };
 
     $scope.testOutcomesTableParams = new ngTableParams({
       page: 1,            // show first page
