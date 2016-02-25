20d6cbf35bf944a814e70a803476a8c87272be87
lauravignoli <vignoli.laura@gmail.com>
2016-02-25 12:45:37 +0200

    BSIS-467 Birth date validation on addDonor is not done properly

--- app/scripts/controllers/donors.js
+++ app/scripts/controllers/donors.js
@@ -149,9 +149,7 @@ angular.module('bsis')
     var minBirthDate = moment().subtract(maxAge, 'years');
     var maxBirthDate = moment().subtract(minAge, 'years');
 
-    function confirmAddDonor(birthDateString) {
-
-      var birthDate = moment(birthDateString, 'YYYY-MM-DD');
+    function confirmAddDonor(birthDate) {
 
       var message;
       if (birthDate.isBefore(minBirthDate)) {
@@ -185,27 +183,34 @@ angular.module('bsis')
       if (valid) {
 
         newDonor.birthDate = dob.year + '-' + dob.month + '-' + dob.dayOfMonth;
+        var birthDate = moment(newDonor.birthDate, 'YYYY-MM-DD');
 
-        confirmAddDonor(newDonor.birthDate).then(function() {
+        if (!moment(birthDate).isValid()) {
+          $scope.dobValid = false;
+          $scope.addingDonor = false;
+        } else {
 
-          $scope.addingDonor = true;
+          confirmAddDonor(birthDate).then(function() {
 
-          DonorService.addDonor(newDonor, function(donor) {
+            $scope.addingDonor = true;
 
-            $scope.format = DATEFORMAT;
-            $scope.initDate = $scope.donor.birthDate;
-            $scope.donorBirthDateOpen = false;
-            $scope.submitted = '';
-            $location.path('/viewDonor/' + donor.id).search({});
-          }, function(err) {
-            $scope.errorMessage = err.userMessage;
-            $scope.err = err;
-            if (err['donor.birthDate']) {
-              $scope.dobValid = false;
-            }
-            $scope.addingDonor = false;
+            DonorService.addDonor(newDonor, function(donor) {
+
+              $scope.format = DATEFORMAT;
+              $scope.initDate = $scope.donor.birthDate;
+              $scope.donorBirthDateOpen = false;
+              $scope.submitted = '';
+              $location.path('/viewDonor/' + donor.id).search({});
+            }, function(err) {
+              $scope.errorMessage = err.userMessage;
+              $scope.err = err;
+              if (err['donor.birthDate']) {
+                $scope.dobValid = false;
+              }
+              $scope.addingDonor = false;
+            });
           });
-        });
+        }
       } else {
         $scope.submitted = true;
       }
