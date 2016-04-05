c4eab27cf04567fc228a786aa4ba53e5942c84f1
Duma Mtungwa <dumakude@gmail.com>
2016-04-05 17:30:06 +0200

    BSIS-701 Fix find donor

--- app/scripts/controllers/donors/findDonors.js
+++ app/scripts/controllers/donors/findDonors.js
@@ -2,7 +2,57 @@
 
 angular.module('bsis')
 
-.controller('FindDonorsCtrl', function() {
+.controller('FindDonorsCtrl', function($scope, $location, $routeParams, ngTableParams, $filter, DonorService, Alerting) {
 
+  $scope.donorSearch = $routeParams;
 
+  var data = [{}];
+  $scope.data = data;
+  $scope.canAddDonors = false;
+
+  // Check that at least one search field is entered
+  $scope.isDonorSearchValid = function() {
+    var search = $scope.donorSearch;
+    return search.firstName || search.lastName || search.donorNumber || search.donationIdentificationNumber;
+  };
+
+  $scope.findDonor = function(form) {
+    if (form && !form.$valid) {
+      return;
+    }
+    $scope.donorSearch.search = true;
+    Alerting.setPersistErrors(false);
+    $location.search($scope.donorSearch);
+    $scope.searching = true;
+    DonorService.findDonor($scope.donorSearch, function(response) {
+      data = response.donors;
+      $scope.searchResults = true;
+      $scope.data = response.donors;
+      $scope.canAddDonors = response.canAddDonors;
+      $scope.searching = false;
+    }, function() {
+      $scope.searchResults = false;
+      $scope.searching = false;
+    });
+  };
+
+  if ($routeParams.search) {
+    $scope.findDonor();
+  }
+
+  $scope.tableParams = new ngTableParams({
+    page: 1,            // show first page
+    count: 8,          // count per page
+    sorting: {}
+  }, {
+    defaultSort: 'asc',
+    counts: [], // hide page counts control
+    total: data.length, // length of data
+    getData: function($defer, params) {
+      var orderedData = params.sorting() ?
+        $filter('orderBy')(data, params.orderBy()) : data;
+      params.total(orderedData.length); // set total for pagination
+      $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+    }
+  });
 });
