e9f738849cdb2da045deab6ea39b6dc149d6ce73
lauravignoli <vignoli.laura@gmail.com>
2016-05-05 15:05:08 +0200

    BSIS-834 Update page and controller to add component batch

--- app/scripts/controllers/components/receiveComponents.js
+++ app/scripts/controllers/components/receiveComponents.js
@@ -1,17 +1,64 @@
 'use strict';
 
 angular.module('bsis')
-  .controller('ReceiveComponentsCtrl', function($scope, $timeout, $filter, ngTableParams) {
+  .controller('ReceiveComponentsCtrl', function($scope, $timeout, $filter, ngTableParams, ComponentBatchService, $location) {
 
-    $scope.boxesList = [];
+    $scope.tabs = [{addDelivery: true}, {viewDelivery: false}];
 
-    $scope.addBox = function(boxTemperatureValue) {
-      $scope.boxesList.push(
-        {
-          temperature: boxTemperatureValue
+    // Add componentBatch variables and methods
+
+    function initFields() {
+      $scope.componentBatch.donationBatch = {'id': ''};
+      $scope.componentBatch.location = {'id': ''};
+      $scope.componentBatch.bloodTransportBoxes = [];
+      $scope.componentBatch.deliveryDate = new Date();
+      $scope.temperature = '';
+      $scope.submitted = false;
+
+    }
+
+    function getComponentBatchFormFields() {
+      ComponentBatchService.getComponentBatchesFormFields(function(response) {
+        if (response !== false) {
+          $scope.processingSites = response.processingSites;
+          $scope.donationBatches = response.donationBatches;
+          $scope.componentBatch = response.addComponentBatchForm;
+          initFields();
         }
+      });
+    }
+
+    $scope.addBox = function(temperature) {
+      $scope.componentBatch.bloodTransportBoxes.push(
+        {temperature: temperature}
       );
     };
+
+    $scope.clearForm = function(addComponentBatchForm) {
+      initFields();
+      addComponentBatchForm.$setPristine();
+      $location.search({});
+    };
+
+    $scope.addComponentBatch = function(addComponentBatchForm) {
+      if (addComponentBatchForm.$valid) {
+        $scope.adding = true;
+        ComponentBatchService.addComponentBatch($scope.componentBatch, function() {
+          $scope.submitted = false;
+          $scope.clearForm(addComponentBatchForm);
+          $scope.tabs.addDelivery = false;
+          $scope.tabs.viewDelivery = true;
+        }, function() {
+          $scope.submitted = true;
+        });
+      } else {
+        $scope.submitted = true;
+      }
+      $scope.adding = false;
+
+    };
+
+    // View componentBatch variables and methods
 
     var data = [
       {
@@ -70,5 +117,9 @@ angular.module('bsis')
         $scope.deliveryFormsTableParams.reload();
       });
     });
+
+    // Execute
+
+    getComponentBatchFormFields();
 
   });
