67d97eaf0cd19fb3540547642ea178a89c241bfe
Dagmar Timler <dagmar.timler@gmail.com>
2016-05-09 10:57:33 +0200

    BSIS-838: Hook up View ComponentBatch page to endpoint
    Retrieve the ComponentBatch from the endpoint and display.

--- app/scripts/services/components.js
+++ app/scripts/services/components.js
@@ -66,6 +66,13 @@ angular.module('bsis')
         }, function() {
           response(false);
         });
+      },
+      getComponentBatch: function(id, onSuccess, onFailure) {
+        Api.ComponentBatches.get({id: id}, function(response) {
+          onSuccess(response);
+        }, function(err) {
+          onFailure(err);
+        });
       }
     };
   });
