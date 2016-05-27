de1c1198458cd7eb8f91dd34355bb4fb32eaef7e
lauravignoli <vignoli.laura@gmail.com>
2016-05-27 11:23:52 +0200

    BSIS-1045 Fix get order forms frontend service declaration

--- app/scripts/services/orderForms.js
+++ app/scripts/services/orderForms.js
@@ -5,7 +5,7 @@ angular.module('bsis').factory('OrderFormsService', function(Api) {
   return {
     getOrderFormsForm: Api.OrderForms.getForm,
     addOrderForm: Api.OrderForms.save,
-    getOrderForm: Api.OrderForms.getForm,
+    getOrderForm: Api.OrderForms.get,
     updateOrderForm: Api.OrderForms.update,
     getOrderFormItemForm: Api.OrderForms.getItemsForm,
     findOrderForms: Api.OrderForms.search
