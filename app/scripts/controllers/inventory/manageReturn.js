c80b3452fdb0476d029309b27d8f64ee7971d5d2
Dagmar Timler <dagmar.timler@gmail.com>
2016-06-13 15:38:06 +0200

    BSIS-1027: Add frontend validation for Record Return Add a popup for error messages when adding a pack to a Return Form.

--- app/scripts/controllers/inventory/manageReturn.js
+++ app/scripts/controllers/inventory/manageReturn.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('bsis').controller('ManageReturnCtrl', function($scope, $location, $log, $routeParams, ReturnFormsService, ComponentService) {
+angular.module('bsis').controller('ManageReturnCtrl', function($scope, $location, $uibModal, $log, $routeParams, ReturnFormsService, ComponentService) {
 
   var componentMaster = {
     din: null,
@@ -89,6 +89,23 @@ angular.module('bsis').controller('ManageReturnCtrl', function($scope, $location
     }, $log.error);
   }
 
+  function showErrorMessage(errorMessage) {
+    $uibModal.open({
+      animation: false,
+      templateUrl: 'views/errorModal.html',
+      controller: 'ErrorModalCtrl',
+      resolve: {
+        errorObject: function() {
+          return {
+            title: 'Invalid Component',
+            button: 'OK',
+            errorMessage: errorMessage
+          };
+        }
+      }
+    });
+  }
+
   $scope.gridOptions = {
     data: [],
     paginationPageSize: 10,
@@ -116,14 +133,26 @@ angular.module('bsis').controller('ManageReturnCtrl', function($scope, $location
       componentCode: $scope.component.componentCode
     };
     ComponentService.findComponent(searchParams, function(component) {
-      $scope.returnForm.components.push(component);
-      populateGrid($scope.returnForm);
-      // reset the form
-      $scope.component = angular.copy(componentMaster);
-      $scope.addComponentForm.$setPristine();
+      // check if component has already been added
+      var componentAlreadyAdded = $scope.returnForm.components.some(function(e) {
+        return e.id === component.id;
+      });
+      if (componentAlreadyAdded) {
+        showErrorMessage('Component ' + $scope.component.din + ' (' + $scope.component.componentCode +
+          ') has already been added to this Return Form.');
+      } else {
+        $scope.returnForm.components.push(component);
+        populateGrid($scope.returnForm);
+        $scope.component = angular.copy(componentMaster); // reset the form
+        $scope.addComponentForm.$setPristine();
+      }
       $scope.addingComponent = false;
     }, function(err) {
-      $log.error(err);
+      if (err.errorCode === 'NOT_FOUND') {
+        showErrorMessage('Component with DIN ' + $scope.component.din + ' and ComponentCode ' + $scope.component.componentCode + ' not found.');
+      } else {
+        $log.error(err);
+      }
       $scope.addingComponent = false;
     });
   };
