9c32c817dd6da4d814860e9f8b34c6c217077ab0
lauravignoli <vignoli.laura@gmail.com>
2016-07-18 16:19:34 +0200

    BSIS-1277 Implement editing a location
    Handle route parameter so the location can be fetched and edited.

--- app/scripts/controllers/settings/manageLocation.js
+++ app/scripts/controllers/settings/manageLocation.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('bsis').controller('ManageLocationCtrl', function($scope, $location, $log, LocationsService, $timeout) {
+angular.module('bsis').controller('ManageLocationCtrl', function($scope, $location, $log, LocationsService, $timeout, $routeParams) {
 
   $scope.location = {};
   $scope.locationForm = {};
@@ -22,20 +22,43 @@ angular.module('bsis').controller('ManageLocationCtrl', function($scope, $locati
     }
 
     $scope.savingLocation = true;
-    LocationsService.addLocation($scope.location, function(reponse) {
-      $log.info('Saved location: ' + reponse.name);
-      //$location.path('/manageLocations');
-      $scope.savingLocation = false;
-    }, function(response) {
-      if (response.data && response.data.name) {
-        $scope.locationForm.name.$setValidity('duplicate', false);
-      }
-      $scope.savingLocation = false;
-    });
+    if ($routeParams.id) {
+      LocationsService.updateLocation($scope.location, function(reponse) {
+        $log.info('Updated location: ' + reponse.name);
+        //$location.path('/manageLocations');
+        $scope.savingLocation = false;
+      }, function(response) {
+        if (response.data && response.data.name) {
+          $scope.locationForm.name.$setValidity('duplicate', false);
+        }
+        $scope.savingLocation = false;
+      });
+    } else {
+      LocationsService.addLocation($scope.location, function(reponse) {
+        $log.info('Saved location: ' + reponse.name);
+        //$location.path('/manageLocations');
+        $scope.savingLocation = false;
+      }, function(response) {
+        if (response.data && response.data.name) {
+          $scope.locationForm.name.$setValidity('duplicate', false);
+        }
+        $scope.savingLocation = false;
+      });
+    }
   };
 
   $scope.validateLocationType = function() {
     return $scope.location.isVenue || $scope.location.isUsageSite || $scope.location.isProcessingSite || $scope.location.isDistributionSite || $scope.location.isTestingSite;
   };
+
+  function init() {
+    if ($routeParams.id) {
+      LocationsService.getLocationById({id: $routeParams.id}, function(response) {
+        $scope.location = response.location;
+      }, $log.error);
+    }
+  }
+
+  init();
 
 });
--- app/scripts/services/locations.js
+++ app/scripts/services/locations.js
@@ -20,20 +20,7 @@ angular.module('bsis')
       },
 
       addLocation: Api.Locations.save,
-      updateLocation: function(location, onSuccess, onError) {
-
-        var updateLocation = Api.Locations.get({id: location.id}, function() {
-          updateLocation = updateLocation.location;
-          angular.copy(location, updateLocation);
-
-          Api.Locations.update({id: location.id}, updateLocation, function(data) {
-            onSuccess(data);
-          }, function(err) {
-            onError(err.data);
-          });
-
-        });
-      },
+      updateLocation: Api.Locations.update,
       search: Api.Locations.search,
       getLocationById: Api.Locations.get
     };
