851bf5d510b8eafd252ccedffde3ff816a51d869
lauravignoli <vignoli.laura@gmail.com>
2016-10-05 17:39:55 +0200

    Revert: BSIS-1806 Add error message when err.data is null
    Add error message in case where err.data returned from server
    is null when calling DataExport endpoint. (reverted from commit cac7244f50eaa915b4891da9a798be1482e3a9d4)

--- app/scripts/controllers/settings/dataExport.js
+++ app/scripts/controllers/settings/dataExport.js
@@ -19,7 +19,7 @@ angular.module('bsis').controller('DataExportCtrl', function($scope, $log, Modal
 
     }, function(err) {
       $log.error(err);
-      if (err && err.userMessage) {
+      if (err.userMessage) {
         $scope.error = {
           message: err.userMessage
         };
--- app/scripts/services/dataExport.js
+++ app/scripts/services/dataExport.js
@@ -6,15 +6,9 @@ angular.module('bsis').factory('DataExportService', function(Api) {
     getDataExport: function(onSuccess, onError) {
       return Api.DataExport.download().then(onSuccess, function(err) {
         // decode the arraybuffer into the error data object returned by the server
-        var errData;
-        if (err.data) {
-          var decodedErrData = String.fromCharCode.apply(null, new Uint8Array(err.data));
-          errData = angular.fromJson(decodedErrData);
-          onError(errData);
-        } else {
-          errData = {userMessage:'An unkown error occurred.'};
-          onError(errData);
-        }
+        var decodedErrData = String.fromCharCode.apply(null, new Uint8Array(err.data));
+        var errData = angular.fromJson(decodedErrData);
+        onError(errData);
       });
     }
 
