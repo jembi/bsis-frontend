4d076eb42185c6128c42a51684ae4cfb6b18f468
lauravignoli <vignoli.laura@gmail.com>
2016-11-07 14:02:59 +0200

    BSIS-2021 Create dataSort service unit tests
    Updated the original method to cover all edge cases.

--- app/scripts/services/utils.js
+++ app/scripts/services/utils.js
@@ -4,12 +4,14 @@ angular.module('bsis')
   .factory('UtilsService', function() {
     return {
       dateSort: function(date1, date2) {
-        if (date2 === null || date1 < date2) {
-          return -1; // if date2 is null it never expires (so is larger than date1)
+        if (date1 === null && date2 === null) {
+          return 0; // equal and null
+        } else if (date2 === null || (date1 !== null && date1 < date2)) {
+          return -1; // if date2 is null it's consider larger than date1
         } else if (date1 === null || date1 > date2) {
-          return 1;
+          return 1; // if date1 is null it's consider larger than date2
         } else {
-          return 0; // equal
+          return 0; // equal and not null
         }
       }
     };
