84fcbea0bcb5babf025f6178e1b4e39605f8624b
lauravignoli <vignoli.laura@gmail.com>
2016-11-10 09:52:31 +0200

    BSIS-1919 Only show the summary section if venues > 0
    Applied to Donations Type Report.

--- app/scripts/controllers/reports/donationTypesReport.js
+++ app/scripts/controllers/reports/donationTypesReport.js
@@ -143,6 +143,7 @@ angular.module('bsis')
 
         // New venue
         if (newRow.location.name !== previousVenue) {
+          $scope.venuesNumber += 1;
 
           if (previousVenue != '') {
             // Add female, male and all rows for previous venue
@@ -190,9 +191,12 @@ angular.module('bsis')
 
       ReportsService.generateDonationsReport(period, function(report) {
         $scope.searching = false;
+        $scope.venuesNumber = 0;
         if (report.dataValues.length > 0) {
           mergeData(report.dataValues);
           $scope.gridOptions.paginationCurrentPage = 1;
+        } else {
+          $scope.gridOptions.data = [];
         }
         $scope.submitted = true;
       }, function(err) {
@@ -227,14 +231,22 @@ angular.module('bsis')
 
       // PDF header
       exporterPdfHeader: function() {
-        return ReportsLayoutService.generatePdfPageHeader($scope.gridOptions.exporterPdfOrientation,
+        var noDataForRangeLine = '';
+        if ($scope.venuesNumber === 0) {
+          noDataForRangeLine = 'No data for date range selected';
+        }
+        var header =  ReportsLayoutService.generatePdfPageHeader($scope.gridOptions.exporterPdfOrientation,
           'Donations Collected By Type Report',
-          ['Date Period: ', $filter('bsisDate')($scope.search.startDate), ' to ', $filter('bsisDate')($scope.search.endDate)]);
+          ['Date Period: ', $filter('bsisDate')($scope.search.startDate), ' to ', $filter('bsisDate')($scope.search.endDate)],
+          noDataForRangeLine);
+        return header;
       },
 
       // Change formatting of PDF
       exporterPdfCustomFormatter: function(docDefinition) {
-        docDefinition = ReportsLayoutService.addSummaryContent(calculateSummary(), docDefinition);
+        if ($scope.venuesNumber > 0) {
+          docDefinition = ReportsLayoutService.addSummaryContent(calculateSummary(), docDefinition);
+        }
         docDefinition = ReportsLayoutService.highlightTotalRows('All', 1, docDefinition);
         docDefinition = ReportsLayoutService.highlightPercentageRows('%', 1, docDefinition);
         docDefinition = ReportsLayoutService.paginatePdf(44, docDefinition);
@@ -243,7 +255,7 @@ angular.module('bsis')
 
       // PDF footer
       exporterPdfFooter: function(currentPage, pageCount) {
-        return ReportsLayoutService.generatePdfPageFooter('venues', $scope.gridOptions.data.length / 4, currentPage, pageCount, $scope.gridOptions.exporterPdfOrientation);
+        return ReportsLayoutService.generatePdfPageFooter('venues', $scope.venuesNumber, currentPage, pageCount, $scope.gridOptions.exporterPdfOrientation);
       },
 
       onRegisterApi: function(gridApi) {
