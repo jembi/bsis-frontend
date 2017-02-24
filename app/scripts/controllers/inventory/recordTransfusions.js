0bfec6f000f3b3571dfc7cd2076b2c9aa67b8adc
Judge Muzinda <jmuzinda@gmail.com>
2017-02-24 09:29:30 +0200

    BSIS-2660 Add transfusion service to index.html and update model refs
    Add transfusion service to index.html so that it is available to the application
    and add references to page models so that they read values returned from the backend.

--- app/scripts/controllers/inventory/recordTransfusions.js
+++ app/scripts/controllers/inventory/recordTransfusions.js
@@ -1,25 +1,15 @@
 'use strict';
 
-angular.module('bsis').controller('RecordTransfusionsCtrl', function($scope, $log, DATEFORMAT) {
-
-  // Set up mock data
-  var componentTypes = [{'id':1, 'componentTypeName':'Whole Blood Single Pack - CPDA', 'componentTypeCode':'0011', 'description':'', 'maxBleedTime':null, 'maxTimeSinceDonation':null},
-                          {'id':2, 'componentTypeName':'Whole Blood Double Pack - CPDA', 'componentTypeCode':'0012', 'description':'', 'maxBleedTime':null, 'maxTimeSinceDonation':null},
-                          {'id':3, 'componentTypeName':'Whole Blood Triple Pack - CPDA', 'componentTypeCode':'0013', 'description':'', 'maxBleedTime':null, 'maxTimeSinceDonation':null},
-                          {'id':4, 'componentTypeName':'Whole Blood Quad Pack - CPDA', 'componentTypeCode':'0014', 'description':'', 'maxBleedTime':null, 'maxTimeSinceDonation':null}];
-  var locations = [{'id':1, name:'St.matome hospital'},
-                    {'id':2, name:'Groote schuur'},
-                    {'id':3, name:'Parakoane hospital'},
-                    {'id':3, name:'TygerValley hospital'}];
-
-  var transfusionOutcomes = ['TRANSFUSED_UNEVENTFULLY', 'TRANSFUSION_REACTION_OCCURRED', 'NOT_TRANSFUSED'];
-  var transfusionReactionTypes = [{'id':1, 'type': 'Reacted'}, {'id':2, 'type': 'Not Reacted'}];
-  var genders = ['male', 'female'];
-  var bloodGroups = ['A+', 'A-', 'B+', 'B-', 'O-', 'O+', 'AB'];
-
-  $scope.today = moment().startOf('day').toDate();
+angular.module('bsis').controller('RecordTransfusionsCtrl', function($scope, $log, TransfusionService, DATEFORMAT) {
 
   $scope.submitted = false;
+  $scope.componentTypes = null;
+  $scope.locations = null;
+  $scope.transfusionOutcomes = null;
+  $scope.transfusionReactionTypes = null;
+  $scope.genders = null;
+  $scope.bloodGroups = null;
+  $scope.today = moment().startOf('day').toDate();
   $scope.transfusion = {
     donationIdentificationNumber: null,
     componentCode: null,
@@ -35,13 +25,17 @@ angular.module('bsis').controller('RecordTransfusionsCtrl', function($scope, $lo
   };
 
   function initializeRecordTransfusionForm() {
-    $scope.format = DATEFORMAT;
-    $scope.componentTypes = componentTypes;
-    $scope.locations = locations;
-    $scope.transfusionOutcomes = transfusionOutcomes;
-    $scope.genders = genders;
-    $scope.transfusionReactionTypes = transfusionReactionTypes;
-    $scope.bloodGroups = bloodGroups;
+    TransfusionService.getTransfusionForm(function(response) {
+      if (response !== false) {
+        $scope.format = DATEFORMAT;
+        $scope.componentTypes = response.componentTypes;
+        $scope.locations = response.usageSites;
+        $scope.transfusionOutcomes = response.transfusionOutcomes;
+        $scope.genders = response.genders;
+        $scope.transfusionReactionTypes = response.transfusionReactionTypes;
+        $scope.bloodGroups = response.bloodGroups;
+      }
+    });
   }
 
   $scope.clearTransfusionReactionType = function() {
