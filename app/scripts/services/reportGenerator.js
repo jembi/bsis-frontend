d8348f048fc30df0617cd5d1109e995770870f0f
Dagmar Timler <dagmar.timler@gmail.com>
2017-03-06 09:54:26 +0200

    BSIS-2833: Minor change to ReportsGen Service
    Minor change to ReportsGeneratorService to rename one parameter so that
    it is more generic (reactionTypes to dynamicData).

--- app/scripts/services/reportGenerator.js
+++ app/scripts/services/reportGenerator.js
@@ -10,11 +10,11 @@ angular.module('bsis').factory('ReportGeneratorService', function($filter) {
 
     // generate the rows of the report/table
     // param: dataValues: an array of the data values provided by the report
-    // param: reactionTypes: an array of Transfusion Reaction Types, which can be configured by the user
+    // param: dynamicData: an array of dynamic data, which can be configured by the user and is used to init and populate data rows (can be null)
     // param: initRow: a function that initialises and returns a single table row object
     // param: populateRow: a function that, given row and data value objects, updates the row with the counts from the data value
     // returns: A response object with the first element containing the generated rows, and the second element, the number of locations counted
-    generateDataRowsGroupingByLocation: function(dataValues, reactionTypes, initRow, populateRow) {
+    generateDataRowsGroupingByLocation: function(dataValues, dynamicData, initRow, populateRow) {
       var response = [];
       var generatedRows = [];
       var locationsNumber = 0;
@@ -25,7 +25,7 @@ angular.module('bsis').factory('ReportGeneratorService', function($filter) {
         rowForLocation = rowsForLocation[dataValue.location.name];
         if (!rowForLocation) { // new location
           locationsNumber += 1;
-          rowForLocation = initRow(reactionTypes);
+          rowForLocation = initRow(dynamicData);
           rowsForLocation[dataValue.location.name] = rowForLocation;
         }
         populateRow(rowForLocation, dataValue);
@@ -42,12 +42,12 @@ angular.module('bsis').factory('ReportGeneratorService', function($filter) {
 
     // generate the summary rows for the report/table
     // param: dataValues: an array of the data values provided by the report
-    // param: reactionTypes: an array of Transfusion Reaction Types, which can be configured by the user
+    // param: dynamicData: an array of dynamic data, which can be configured by the user and is used to init and populate data rows (can be null)
     // param: initRow: a function that initialises and returns a single table row object
     // param: populateRow: a function that, given row and data value objects, updates the row with the counts from the data value
     // returns: a summary row object that can be appended to the report/table
-    generateSummaryRow: function(dataValues, reactionTypes, initRow, populateRow) {
-      var summaryRow = initRow(reactionTypes);
+    generateSummaryRow: function(dataValues, dynamicData, initRow, populateRow) {
+      var summaryRow = initRow(dynamicData);
       angular.forEach(dataValues, function(dataValue) {
         populateRow(summaryRow, dataValue);
       });
