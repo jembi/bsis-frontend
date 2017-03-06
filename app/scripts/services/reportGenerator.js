2366861d5475651457d47ada3f38399837ef7154
Dagmar Timler <dagmar.timler@gmail.com>
2017-03-06 09:51:19 +0200

    BSIS-2833: Add documentation for ReportGen Service
    Added some documentation for the ReportGeneratorService

--- app/scripts/services/reportGenerator.js
+++ app/scripts/services/reportGenerator.js
@@ -8,7 +8,12 @@ angular.module('bsis').factory('ReportGeneratorService', function($filter) {
       return $filter('filter')(dataValue.cohorts, { category: category})[0];
     },
 
-    // The response's first element is generated rows, and the second element is the number of usage sites
+    // generate the rows of the report/table
+    // param: dataValues: an array of the data values provided by the report
+    // param: reactionTypes: an array of Transfusion Reaction Types, which can be configured by the user
+    // param: initRow: a function that initialises and returns a single table row object
+    // param: populateRow: a function that, given row and data value objects, updates the row with the counts from the data value
+    // returns: A response object with the first element containing the generated rows, and the second element, the number of locations counted
     generateDataRowsGroupingByLocation: function(dataValues, reactionTypes, initRow, populateRow) {
       var response = [];
       var generatedRows = [];
@@ -35,6 +40,12 @@ angular.module('bsis').factory('ReportGeneratorService', function($filter) {
       return response;
     },
 
+    // generate the summary rows for the report/table
+    // param: dataValues: an array of the data values provided by the report
+    // param: reactionTypes: an array of Transfusion Reaction Types, which can be configured by the user
+    // param: initRow: a function that initialises and returns a single table row object
+    // param: populateRow: a function that, given row and data value objects, updates the row with the counts from the data value
+    // returns: a summary row object that can be appended to the report/table
     generateSummaryRow: function(dataValues, reactionTypes, initRow, populateRow) {
       var summaryRow = initRow(reactionTypes);
       angular.forEach(dataValues, function(dataValue) {
