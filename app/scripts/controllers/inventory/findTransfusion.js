f7355fa9a5c9d9a3b4e040d7dcf665a7c6510c63
Judge Muzinda <jmuzinda@gmail.com>
2017-03-06 14:22:41 +0200

    BSIS-2820 Add findTransfusion view and controller
    Add findTransfusion view and controller that will be used to search for transfusions.

---
+++ app/scripts/controllers/inventory/findTransfusion.js
@@ -0,0 +1,97 @@
+'use strict';
+
+angular.module('bsis')
+  .controller('FindTransfusionCtrl', function($scope, $filter, $log, DATEFORMAT) {
+
+    $scope.dateFormat = DATEFORMAT;
+    $scope.transfusionOutcomes = [];
+    $scope.componentTypes = [];
+    $scope.transfusionSites = [];
+
+    $scope.searchParams = {};
+    $scope.submitted = false;
+    $scope.searching = false;
+
+    var master = {
+      donationIdentificationNumber: null,
+      componentCode: null,
+      componentTypeId: null,
+      receivedFromId: null,
+      transfusionOutcome: null,
+      startDate: null,
+      endDate: null
+    };
+
+    var columnDefs = [
+      {
+        name: 'DIN',
+        displayName: 'DIN',
+        field: 'donationIdentificationNumber',
+        width: '**',
+        maxWidth: '150'
+      },
+      {
+        name: 'Component Code',
+        field: 'componentCode',
+        width: '**',
+        maxWidth: '150'
+      },
+      {
+        name: 'Component Type',
+        field: 'componentType.componentTypeName',
+        width: '**'
+      },
+      {
+        name: 'Transfused On',
+        field: 'dateTransfused',
+        cellFilter: 'bsisDate',
+        width: '**',
+        maxWidth: '150'
+      },
+      {
+        name: 'Outcome',
+        field: 'transfusionOutcome',
+        width: '**'
+      }
+    ];
+
+    $scope.gridOptions = {
+      data: [],
+      paginationPageSize: 8,
+      paginationPageSizes: [10],
+      paginationTemplate: 'views/template/pagination.html',
+      minRowsToShow: 8,
+      columnDefs: columnDefs,
+      onRegisterApi: function(gridApi) {
+        $scope.gridApi = gridApi;
+      }
+    };
+
+    function initialiseForm() {
+      $scope.searchParams = angular.copy(master);
+      $scope.submitted = false;
+      $scope.searching = false;
+      // initialize form models here
+      $log.debug('Form initialised');
+    }
+
+    $scope.findTransfusion = function(searchForm) {
+      if (searchForm.$invalid) {
+        return;
+      }
+      $scope.submitted = true;
+      $scope.searching = true;
+      // hook up post endpoint
+      $log.debug('Form initialised');
+    };
+
+    $scope.clearSearch = function(searchForm) {
+      $scope.gridOptions.data = [];
+      $scope.searchParams = angular.copy(master);
+      $scope.submitted = false;
+      $scope.searching = false;
+      searchForm.$setPristine();
+    };
+
+    initialiseForm();
+  });
