e32d03ec88d3a48e677b7768a006e443887321b3
tumijacob <matome.phoshoko@jembi.org>
2017-03-07 14:56:12 +0200

    Revert "BSIS-2720 Add the summary part of the blood units issued report"
    This reverts commit 841749699f94b649fb62bf1ddafc2fe12bb34dc7.

--- app/scripts/controllers/reports/bloodUnitsIssuedReport.js
+++ app/scripts/controllers/reports/bloodUnitsIssuedReport.js
@@ -18,7 +18,6 @@ angular.module('bsis')
     function initialise() {
       ReportsService.getBloodUnitsIssuedReportForm(function(response) {
         componentTypes = response.componentTypes;
-        $scope.distributionSitesNumber = 0;
       }, $log.error);
     }
 
@@ -121,18 +120,9 @@ angular.module('bsis')
         if (report.dataValues.length > 0) {
           var data = ReportGeneratorService.generateDataRowsGroupingByLocation(report.dataValues, componentTypes, initRow, mergeData);
           $scope.gridOptions.data = data[0];
-          $scope.distributionSitesNumber = data[1];
-
-          // Add summary row
-          if ($scope.distributionSitesNumber > 1) {
-            var summaryRow = ReportGeneratorService.generateSummaryRow(report.dataValues, componentTypes, initRow, mergeData);
-            summaryRow.distributionSite = 'All Sites';
-            $scope.gridOptions.data.push(summaryRow);
-          }
           $scope.gridOptions.paginationCurrentPage = 1;
         } else {
           $scope.gridOptions.data = [];
-          $scope.distributionSitesNumber = 0;
         }
         $scope.submitted = true;
       }, function(err) {
@@ -183,16 +173,15 @@ angular.module('bsis')
 
       // PDF header
       exporterPdfHeader: function() {
-        var distributionSitesNumberLine = 'Distribution Sites: ' + $scope.distributionsSitesNumber;
         var header =  ReportsLayoutService.generatePdfPageHeader($scope.gridOptions.exporterPdfOrientation,
           'Blood Units Issued Summary Report',
-          ['Date Period: ', $filter('bsisDate')($scope.search.startDate), ' to ', $filter('bsisDate')($scope.search.endDate)], distributionSitesNumberLine);
+          ['Date Period: ', $filter('bsisDate')($scope.search.startDate), ' to ', $filter('bsisDate')($scope.search.endDate)]);
         return header;
       },
 
       // PDF footer
       exporterPdfFooter: function(currentPage, pageCount) {
-        return ReportsLayoutService.generatePdfPageFooter('sites', $scope.distributionSitesNumber, currentPage, pageCount, $scope.gridOptions.exporterPdfOrientation);
+        return ReportsLayoutService.generatePdfPageFooter(null, null, currentPage, pageCount, $scope.gridOptions.exporterPdfOrientation);
       },
 
       onRegisterApi: function(gridApi) {
