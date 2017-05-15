bd583d8a0f5ffe1d1bb54e3b2d383a44f3e01afa
Patience Mpofu <patience.mpofu@jembi.org>
2017-05-15 11:55:21 +0200

    BSIS-3239 Corrected params translation

--- app/scripts/controllers/components/viewComponentBatch.js
+++ app/scripts/controllers/components/viewComponentBatch.js
@@ -97,7 +97,7 @@ angular.module('bsis').controller('ViewComponentBatchCtrl', function($scope, $ro
     // PDF footer
     exporterPdfFooter: function(currentPage, pageCount) {
       var columns = [
-        {text: gettextCatalog.getString('Number of components') + ': '  + $scope.gridOptions.data.length, width: 'auto'},
+        {text: gettextCatalog.getString('Number of components: {{componentNumber}} ', {componentNumber: $scope.gridOptions.data.length}), width: 'auto'},
         {text: 'Date generated: ' + $filter('bsisDateTime')(new Date()), width: 'auto'},
         {text: 'Page ' + currentPage + ' of ' + pageCount, style: {alignment: 'right'}}
       ];
